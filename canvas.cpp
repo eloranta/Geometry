47a74213432fceed27e1e1fdd9aeb71277f2ebaa
eloranta <hannu.eloranta@gmail.com>
2025-12-22 17:10:45 +0200

    add handler for resize

--- canvas.cpp
+++ canvas.cpp
@@ -1,12 +1,36 @@
+#include <QMouseEvent>
 #include "canvas.h"
 
 Canvas::Canvas(QWidget *parent)
     : QWidget{parent}
 {
     setAttribute(Qt::WA_StyledBackground, true);
+    updateTransform();
 }
 
 void Canvas::mousePressEvent(QMouseEvent *event) {
-    qDebug() << "press";
+    QPointF point = screenToCanvas(event->position());
+
+    qDebug() << point;
     QWidget::mousePressEvent(event);
+}
+
+void Canvas::resizeEvent(QResizeEvent *event)
+{
+    updateTransform();
+    QWidget::resizeEvent(event);
+}
+
+void Canvas::updateTransform()
+{
+    const int padding = 16;
+    QRectF area = rect().adjusted(padding, padding, -padding, -padding);
+    const double span = 10.0;
+    if (area.width() <= 0.0 || area.height() <= 0.0) {
+        scale = 1.0;
+        origin = QPointF(0.0, 0.0);
+        return;
+    }
+    scale = std::min(area.width(), area.height()) / span;
+    origin = QPointF(area.left() + area.width() / 2.0, area.top() + area.height() / 2.0);
 }
--- canvas.h
+++ canvas.h
@@ -11,6 +11,17 @@ public:
 protected:
     // void paintEvent(QPaintEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+private:
+    double scale;
+    QPointF origin;
+    void updateTransform();
+    QPointF CanvasToScreen(const QPointF &point){
+        return QPointF(origin.x() + point.x() * scale, origin.y() - point.y() * scale);
+    };
+    QPointF screenToCanvas(const QPointF &point){
+        return QPointF((point.x() - origin.x()) / scale, -(point.y() - origin.y()) / scale);
+    };
 
 signals:
 };
