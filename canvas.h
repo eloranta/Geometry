da2ec75d07c259a26ea73a3cb76e3b5a67b9ab07
eloranta <hannu.eloranta@gmail.com>
2025-12-23 16:00:56 +0200

    add object classes

--- canvas.h
+++ canvas.h
@@ -6,6 +6,35 @@
 class Canvas : public QWidget
 {
     Q_OBJECT
+private:
+    struct Object {
+        QString label;
+        explicit Object(const QString &label = QString()) : label(label) {}
+        virtual ~Object() = default;
+    };
+    struct Point : public Object {
+        QPointF positiom;
+        Point() = default;
+        Point(const QPointF &point, const QString &label) : Object(label), positiom(point) {}
+    };
+    struct Line : public Object {
+        int a = -1;
+        int b = -1;
+        Line() = default;
+        Line(int a, int b, const QString &label) : Object(label), a(a), b(b) {}
+    };
+    struct ExtendedLine : public Object {
+        QPointF a;
+        QPointF b;
+        ExtendedLine() = default;
+        ExtendedLine(const QPointF &a, const QPointF &b, const QString &label) : Object(label), a(a), b(b) {}
+    };
+    struct Circle : public Object {
+        QPointF center;
+        double radius = 0.0;
+        Circle() = default;
+        Circle(const QPointF &center, double radius, const QString &label = QString()) : Object(label), center(center), radius(radius) {}
+    };
 public:
     explicit Canvas(QWidget *parent = nullptr);
 protected:
